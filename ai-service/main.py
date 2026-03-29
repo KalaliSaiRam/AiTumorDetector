a546800242baa89db94f5884c726905dfa3a6b39
r-a-m-25 <ram.goud2524@gmail.com>
2026-03-30 00:23:28 +0530

    Final changes

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, UploadFile, File  # type: ignore
+from fastapi.middleware.cors import CORSMiddleware
 import sys
 import os
+import uvicorn
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from inference import predict_image  # type: ignore
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -15,3 +25,7 @@ def read_root():
 async def predict(file: UploadFile = File(...)):
     result = predict_image(file)
     return result
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
